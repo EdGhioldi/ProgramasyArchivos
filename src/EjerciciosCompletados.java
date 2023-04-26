91e1c6022900beb8a3a6ca2f441b4813e965b7f7
Eduardo Ghioldi <ghioldigahonaeduardo@gmail.com>
2023-04-26 01:41:19 -0300

    Ejercicio 1.b Completado Creación nueva Clase para EjericiosCompletados.

---
+++ src/EjerciciosCompletados.java
@@ -0,0 +1,41 @@
+public class EjerciciosCompletados {
+// EJERCICIO 1.A
+    public static void main(String[] args) {
+        /*
+        Haga un main, donde por parámetro ponga 3 números y una letra que
+        represente ascendente o descendente y los muestre ordenados por tal criterio.
+        */
+        int num1 = 3; int num2 = 1; int num3 = 2; String letra = "A";
+        int[] numeros = {num1 , num2, num3};
+
+        if (letra.equals("A")) {
+            for (int i = 0; i < numeros.length - 1; i++) {
+                for (int j = 0; j < numeros.length - i - 1; j++) {
+                    if (numeros[j] > numeros[j+1]) {
+                        int temp = numeros[j];
+                        numeros[j] = numeros[j+1];
+                        numeros[j+1] = temp;
+                    }
+                }
+            }
+        } else if (letra.equals("D")) {
+            for (int i = 0; i < numeros.length - 1; i++) {
+                for (int j = 0; j < numeros.length - i - 1; j++) {
+                    if (numeros[j] < numeros[j+1]) {
+                        int temp = numeros[j];
+                        numeros[j] = numeros[j+1];
+                        numeros[j+1] = temp;
+                    }
+                }
+            }
+        }
+
+        System.out.print("Los números ordenados son: ");
+        for (int num : numeros) {
+            System.out.print(num + " ");
+        }
+    }
+
+
+
+}
