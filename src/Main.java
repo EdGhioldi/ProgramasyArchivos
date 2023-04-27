363622f4d65495116808abc40de92e2330ab7718
Eduardo Ghioldi <ghioldigahonaeduardo@gmail.com>
2023-04-27 09:10:58 -0300

    Ejercicio 1.C Corregido y Simplificado

--- src/Main.java
+++ src/Main.java
@@ -1,25 +1,24 @@
     import org.jetbrains.annotations.NotNull;
     import java.util.Scanner;
     public class Main {
+        static boolean falla = true;
         public static void main(String[] args) {
-        boolean falla = false;
             int num1 = 3;
             int num2 = 2;
             int num3 = 43;
             String letra1 = "l";
             int[] Valores = {num1,num2,num3};
     
-            try {
+
                 ValoresPreIngresados(Valores[0], Valores[1], Valores[2], letra1);
-                if (falla == false) {
-                    System.out.println("Vamos Central!");
-                }else {
-                    System.out.println("Error en los Parametros De Comando");
+                if (falla != false) {
+
+                    System.out.println("Codigo Ejecutado Exitosamente");
+
+                }else if (falla != true){
                     ValoresPorConsola(new Scanner(System.in));
                 }
-            } catch (Exception e) {
-                ValoresPorConsola(new Scanner(System.in));
-            }
+
     
     
     
@@ -29,7 +28,7 @@
     
             try {
                 int[] numeros = {num1, num2, num3};
-                if (letra.equals("A")) {
+                if (letra.equalsIgnoreCase("A")) {
                     for (int i = 0; i < numeros.length - 1; i++) {
                         for (int j = 0; j < numeros.length - i - 1; j++) {
                             if (numeros[j] > numeros[j + 1]) {
@@ -40,7 +39,11 @@
                         }
                     }
 
-                } else if (letra.equals("D")) {
+                    System.out.print("Los números ordenados son: ");
+                    for (int num : numeros) {
+                        System.out.print(num + " ");
+                    }
+                } else if (letra.equalsIgnoreCase("D")) {
                     for (int i = 0; i < numeros.length - 1; i++) {
                         for (int j = 0; j < numeros.length - i - 1; j++) {
                             if (numeros[j] < numeros[j + 1]) {
@@ -54,16 +57,13 @@
                     for (int num : numeros) {
                         System.out.print(num + " ");
                     }
-                }  else {
+
+                }  else{
                     System.out.println("La Letra Ingresada es inválida");
                     boolean falla = true;
-                    ValoresPorConsola(new Scanner(System.in));
                 }
 
-                System.out.print("Los números ordenados son: ");
-                for (int num : numeros) {
-                    System.out.print(num + " ");
-                }
+
 
 
             } catch (Exception e){
@@ -74,59 +74,40 @@
     
     
         public static void ValoresPorConsola (Scanner scanner){
-                System.out.print("Ingrese el primer número: ");
-                int num1;
-                try {
-                    num1 = scanner.nextInt();
-                } catch (Exception e) {
-                    System.out.println("Ha ocurrido un error: " + e.getMessage());
-                    scanner.close();
-                    return;
-                }
-    
-                System.out.print("Ingrese el segundo número: ");
-                int num2;
-                try {
-                    num2 = scanner.nextInt();
-                } catch (Exception e) {
-                    System.out.println("Ha ocurrido un error: " + e.getMessage());
-                    scanner.close();
-                    return;
-                }
-    
-                System.out.print("Ingrese el tercer número: ");
-                int num3;
-                try {
-                    num3 = scanner.nextInt();
-                } catch (Exception e) {
-    
-                    System.out.println("Ha ocurrido un error: " + e.getMessage());
-                    scanner.close();
-                    return;
-                }
+                System.out.print("Ingrese el 1er número: ");
+                int nro1;
+                    nro1 = scanner.nextInt();
+
+                System.out.print("Ingrese el 2do número: ");
+                int nro2;
+                    nro2 = scanner.nextInt();
+
+                System.out.print("Ingrese el 3er número: ");
+                int nro3;
+                    nro3 = scanner.nextInt();
+
                 System.out.print("Ingrese 'A' para orden ascendente o 'D' para orden descendente: ");
                 String letra = scanner.next();
-                scanner.close();
-    
-                int[] numeros = {num1, num2, num3};
+
+                int[] nros = {nro1, nro2, nro3};
     
                 if (letra.equalsIgnoreCase("A")) {
-                    for (int i = 0; i < numeros.length - 1; i++) {
-                        for (int j = 0; j < numeros.length - i - 1; j++) {
-                            if (numeros[j] > numeros[j + 1]) {
-                                int temp = numeros[j];
-                                numeros[j] = numeros[j + 1];
-                                numeros[j + 1] = temp;
+                    for (int i = 0; i < nros.length - 1; i++) {
+                        for (int j = 0; j < nros.length - i - 1; j++) {
+                            if (nros[j] > nros[j + 1]) {
+                                int temp = nros[j];
+                                nros[j] = nros[j + 1];
+                                nros[j + 1] = temp;
                             }
                         }
                     }
                 } else if (letra.equalsIgnoreCase("D")) {
-                    for (int i = 0; i < numeros.length - 1; i++) {
-                        for (int j = 0; j < numeros.length - i - 1; j++) {
-                            if (numeros[j] < numeros[j + 1]) {
-                                int temp = numeros[j];
-                                numeros[j] = numeros[j + 1];
-                                numeros[j + 1] = temp;
+                    for (int i = 0; i < nros.length - 1; i++) {
+                        for (int j = 0; j < nros.length - i - 1; j++) {
+                            if (nros[j] < nros[j + 1]) {
+                                int temp = nros[j];
+                                nros[j] = nros[j + 1];
+                                nros[j + 1] = temp;
                             }
                         }
                     }
@@ -135,7 +116,7 @@
                 }
     
                 System.out.print("Los números ordenados son: ");
-                for (int num : numeros) {
+                for (int num : nros) {
                     System.out.print(num + " ");
                 }
             }
