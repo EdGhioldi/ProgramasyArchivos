890d63fba987a96be0c8f6166ca2557a673fd00f
Eduardo Ghioldi <ghioldigahonaeduardo@gmail.com>
2023-04-28 10:01:16 -0300

    Ejercicio 2.B Armado

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,24 @@
-
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
 
 //Al programa anterior agreguele un parámetro para que la operación pueda ser suma o multiplicación
+
 public class Main {
+
     public static void main(String[] args) {
+        String rutaArchivo = "src\\Numeros.txt";
+
+        Scanner scanner = new Scanner(new File(rutaArchivo)).useDelimiter("[^0-9]+"))
+
+        int suma = 0;
+        while (scanner.hasNext()) {
+            int numero = scanner.nextInt();
+            suma += numero;
+
+            System.out.println("La suma de los números en el archivo es: " + suma);
+
+
+        }
     }
-}
 
