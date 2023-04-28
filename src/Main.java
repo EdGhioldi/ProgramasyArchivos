225128f2fad1bf9222d2b3565a781c55372a7bda
Eduardo Ghioldi <ghioldigahonaeduardo@gmail.com>
2023-04-28 09:55:44 -0300

    Ejercicio 2.B Consigna

--- src/Main.java
+++ src/Main.java
@@ -1,26 +1,8 @@
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.util.Scanner;
 
-// Haga un main donde por parámetro envíe la ruta de un archivo. Ese archivo debe contener
-//números. El programa debe escribir por consola la suma de esos números.
+
+//Al programa anterior agreguele un parámetro para que la operación pueda ser suma o multiplicación
 public class Main {
     public static void main(String[] args) {
-        String rutaArchivo = "src\\Numeros.txt";
-
-        try (Scanner scanner = new Scanner(new File(rutaArchivo)).useDelimiter("[^0-9]+")) {
-            int suma = 0;
-            while (scanner.hasNext()) {
-                int numero = scanner.nextInt();
-                suma += numero;
-            }
-            System.out.println("La suma de los números en el archivo es: " + suma);
-        } catch (FileNotFoundException e) {
-            System.err.println("Archivo no encontrado: " + rutaArchivo);
-            System.exit(1);
-        }
     }
 }
 
-     //   D:\Users\Irene\Desktop\Java\Trabajo Integrador\Clase5-ProgramasyArchivos
-     //   D:\Users\Irene\Desktop\Java\Trabajo Integrador\Clase5-ProgramasyArchivos\src
